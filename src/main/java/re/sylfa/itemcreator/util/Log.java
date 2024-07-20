d26126944b9eb12a60a83aab6e0ab609c839bce5
Sylfare <12037446+Sylfare@users.noreply.github.com>
2024-07-20 12:50:14 +0200

    feat: Load items from items folder

---
+++ src/main/java/re/sylfa/itemcreator/util/Log.java
@@ -0,0 +1,26 @@
+package re.sylfa.itemcreator.util;
+
+import org.bukkit.Bukkit;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+
+public class Log {
+    private static String PREFIX = "[ItemCreator] ";
+
+    public static void log(String message){
+        Log.log(Component.text(message));
+    }
+
+    public static void log(Component message){
+        Bukkit.getConsoleSender().sendMessage(Component.text(PREFIX).append(message));
+    }
+
+    public static void warn(String message){
+        Log.warn(Component.text(message));
+    }
+
+    public static void warn(Component message){
+        log(message.color(NamedTextColor.RED));
+    }
+}
