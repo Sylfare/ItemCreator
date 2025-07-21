bbfe7eb1a206a12a09e224ffd9f965ad2f66ea75
Sylfare <12037446+Sylfare@users.noreply.github.com>
2025-07-21 21:48:30 +0200

    feature: add consumable effects in consumable deserializer

--- src/main/java/re/sylfa/itemcreator/deserializers/components/ConsumableDeserializer.java
+++ src/main/java/re/sylfa/itemcreator/deserializers/components/ConsumableDeserializer.java
@@ -5,7 +5,10 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import io.papermc.paper.datacomponent.item.Consumable;
+import io.papermc.paper.datacomponent.item.consumable.ConsumeEffect;
 import io.papermc.paper.datacomponent.item.consumable.ItemUseAnimation;
+import re.sylfa.itemcreator.ItemCreator;
+import re.sylfa.itemcreator.util.Log;
 import re.sylfa.itemcreator.util.Parsers;
 
 import java.io.IOException;
@@ -25,6 +28,19 @@ public class ConsumableDeserializer extends StdDeserializer<Consumable> {
         Parsers.getFloatNodeValue(node, "consumeSeconds").ifPresent(builder::consumeSeconds);
         Parsers.getBooleanNodeValue(node, "hasConsumeParticles").ifPresent(builder::hasConsumeParticles);
         Parsers.getNodeKeyValue(node, "sound").ifPresent(builder::sound);
+        JsonNode effects = node.get("effects");
+        if(effects != null) {
+            if(effects.isArray()) {
+                for(JsonNode effect : effects) {
+                    ConsumeEffect consumeEffect = ItemCreator.getMapper().convertValue(effect, ConsumeEffect.class);
+                    if(consumeEffect != null) {
+                        builder.addEffect(consumeEffect);
+                    }
+                }
+            } else {
+                Log.error("Consumable effect is not valid");
+            }
+        }
         return builder.build();
     }
 }
